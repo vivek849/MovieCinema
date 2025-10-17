d4ad943fcec876673ee8e949e1a9685c8bf6291e
vivek chaurasiya <vkch849@gmail.com>
2025-10-18 00:24:18 +0530

    initial

---
+++ src/main/java/com/infi/moviecinema/MoviecinemaApplication.java
@@ -0,0 +1,13 @@
+package com.infi.moviecinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviecinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviecinemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infi/moviecinema/MoviecinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infi.moviecinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviecinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
